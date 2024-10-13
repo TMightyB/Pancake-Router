853872f0bea2a5dd32f66021772ee84fcf523353
TMightyB <thetrealtmightyb@gmail.com>
2024-10-12 17:31:03 -0700

    agree

--- AddressList.js
+++ AddressList.js
@@ -1,5 +1,5 @@
 const routerAddress = "0x10ED43C718714eb63d5aA57B78B54704E256024E";
-const fromAddress = "0xd048B4C23Af828E5Be412505A51A8Dd7B37782Dd"; //AIA
+const fromAddress = "0xd048b4c23af828e5be412505a51a8dd7b37782dd"; //AIA
 const toAddress="0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";//WBNB
 
 module.exports={
